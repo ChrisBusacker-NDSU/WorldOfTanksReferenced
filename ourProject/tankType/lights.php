e483de24ff05f5043bd85954fdefeecbdccac7b8
ChrisBusacker-NDSU <busackerc@hotmail.com>
2018-12-03 23:00:07 -0600

    update
    just need to change the tanktype in the query but this is a ordered display for the tanks.

--- ourProject/tankType/lights.php
+++ ourProject/tankType/lights.php
@@ -13,6 +13,27 @@
 
     <h1>lights!</h1>
 
+    <?php
+	// Remember to replace 'username' and 'password'!
+	$conn = oci_connect('busacker', 'Sep771994', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+
+	//put your query here
+	$query = "SELECT Tankname, Country.CountryName, Tanktier FROM Tank_List INNER JOIN Country ON Tank_List.CountryId=Country.CountryId WHERE Tanktype = 'Light' ORDER BY Tanktier";
+	$stid = oci_parse($conn,$query);
+	oci_execute($stid,OCI_DEFAULT);
+
+	//iterate through each row
+	while ($row = oci_fetch_array($stid,OCI_ASSOC)) 
+	{
+    	//iterate through each item in the row and echo it  
+    	foreach ($row as $item)    
+    	{
+        	echo $item.' ';
+    	}   
+	echo '<br/>';}
+	oci_free_statement($stid);
+	oci_close($conn);
+	?>
 
 
   </body>
